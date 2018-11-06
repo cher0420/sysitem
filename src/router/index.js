d92edec07d0f0621bc83aba7f7a9d279447e8f3c
xuxue <xuxue@17shihui.com>
2018-11-06 10:45:59 +0800

    222

--- src/router/index.js
+++ src/router/index.js
@@ -6,37 +6,64 @@ import {MENUS} from "../constants/constants";
 Vue.use(Router)
 let arr = []
 let obj = {}
-for(let v of MENUS){
-  let children = [{
-    path: '/',
-    name: v.id,
-    title:v.chineseName,
-    component: () => import(`../page/${v.path}/index`)
-  }]
-  for(let value of v.children){
-    let childrenObj = {
-      path: value.path,
-      name: value.id,
-      component: () => import(`../page/${v.path}/${value.path}/index`),
-      children:[
-        {
-          path:'/',
-          name:'config',
-          title:value.chineseName,
-          component: ()=>import(`../page/${v.path}/${value.path}/index`),
-        },
-      ]
-    }
-    children = [...children,childrenObj]
+// for(let v of MENUS){
+//   let children = [{
+//     path: '/',
+//     name: v.id,
+//     title:v.chineseName,
+//     component: () => import(`../page/${v.path}/index`)
+//   }]
+//   for(let value of v.children){
+//     let childrenObj = {
+//       path: value.path,
+//       name: value.id,
+//       component: () => import(`../page/${v.path}/${value.path}/index`),
+//       children:[
+//         {
+//           path:'/',
+//           name:'config',
+//           title:value.chineseName,
+//           component: ()=>import(`../page/${v.path}/${value.path}/index`),
+//         },
+//       ]
+//     }
+//     children = [...children,childrenObj]
+//   }
+//   obj={
+//     path:`/${v.id}`,
+//     component:Layout,
+//     name:v.id,
+//     children:children
+//   }
+//   arr.push(obj)
+// }
+arr = [
+  {
+    path:'/bot',
+    component: Layout,
+    name: 'bot',
+    children:[
+      {
+        path:'/',
+        component: () =>import('../page/bot/index'),
+        name: 'bot',
+        children:[
+
+        ]
+      },
+      {
+        path:'config',
+        component: () =>import('../page/bot/config/config/index'),
+        name: 'config',
+      },
+      {
+        path:'config/web',
+        component: () =>import('../page/bot/config/web/index'),
+        name: 'config',
+      }
+    ]
   }
-  obj={
-    path:`/${v.id}`,
-    component:Layout,
-    name:v.id,
-    children:children
-  }
-  arr.push(obj)
-}
+]
 const root = [{
     path: '/',
     component: Layout,
