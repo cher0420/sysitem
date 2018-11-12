2579bcde0c0749cc528e0fd77564884f5c5285e4
Eric <17621869522@163.com>
2018-11-12 11:02:41 +0800

    修正

--- src/page/bot/config/caseStore/createCase.vue
+++ src/page/bot/config/caseStore/createCase.vue
@@ -4,73 +4,104 @@
     <div class="container">
       <div>
         <div class="caseTitle">
-          创建案例标题：
+          案例标题：
         </div>
         <el-input v-model="title" placeholder="请输入标题"></el-input>
 
       </div>
       <div class="caseTitle">
-        创建案例内容：
+        案例内容：
       </div>
-      <quill-editor ref="myTextEditor" v-model="content" :options="editorOption"></quill-editor>
-      <el-button class="editor-btn" type="primary" @click="submit">保存</el-button>
-      <el-button plain size="mini" @click="indexCaseShow">取消</el-button>
+      <div>
+        <script id="ueid" type="text/plain"></script>
+
+        <el-button class="editor-btn" type="primary" @click="getUEContent">保存</el-button>
+        <el-button plain size="mini" @click="indexCaseShow">取消</el-button>
+        <!--<button @click="getUEContent()">获取内容</button>-->
+      </div>
     </div>
   </div>
 </template>
 <script>
-
+  // import UE from './ue.vue';
   import {mapActions} from 'vuex'
 
   export default {
     name: "editCase",
-    props: ['navIndex'],
+ //   props: ['navIndex'],
     components: {
+      UE
       // quillEditor
     },
-    mounted(){
-
-
-    },
+    // mounted(){
+    //   this.title = this.navIndex.title;
+    //
+    // },
     data() {
       return {
+        editor: null,
         title:"",
-        content: '',
-        editorOption: {
-          placeholder: '请输入案例内容！'
+        // content: '',
+        // editorOption: {
+        //   placeholder: '请输入案例内容！'
+        // },
+        uetest:'<p>sss44444444444444ss</p>',
+
+        config: {
+          initialFrameWidth: null,
+          initialFrameHeight: 350
         }
       }
     },
+    mounted() {
+   //   this.title = this.navIndex.title;
+
+      const _this = this;
+      this.editor = UE.getEditor("ueid", this.config); // 初始化UE
+      this.editor.addListener("ready", function () {
+        _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
+      });
+    },
     methods: {
-      onEditorChange({editor, html, text}) {
-        this.content = html;
+      getUEContent() {    // 获取内容方法
+        console.log(this.editor.getContent())
+        return this.editor.getContent()
       },
-      submit() {
-        console.log(this.title)
-        console.log(this.content);
-        this.$alert('是否保存？', '操作提示', {
-          confirmButtonText: '确定',
-          callback: action => {
 
-
-          }
-        });
-      },
+      // onEditorChange({editor, html, text}) {
+      //   this.content = html;
+      // },
+      // submit() {
+      //   console.log(this.title)
+      //   console.log(this.content);
+      //   this.$alert('保存成功', '操作提示', {
+      //     confirmButtonText: '确定',
+      //     callback: action => {
+      //
+      //
+      //     }
+      //   });
+      // },
       ...mapActions(
         ["indexCaseShow"]
       ),
 
+    },
+    destroyed() {
+      this.editor.destroy();
     }
   }
 </script>
 <style scoped>
-  .editor-btn {
-    margin-top: 20px;
-  }
+
   .caseTitle {
     margin: 10px 0;
     font-size: 18px;
     color: #555;
+  }
+
+  .editor-btn {
+    margin-top: 20px;
   }
 </style>
 <style>
--- src/page/bot/config/caseStore/editCase.vue
+++ src/page/bot/config/caseStore/editCase.vue
@@ -12,18 +12,18 @@
       <div class="caseTitle">
         案例内容：
       </div>
-      　<section>
+      <div>
+        <script id="ueid" type="text/plain"></script>
 
-      　　　　<UE :defaultMsg='uetest' :config=config ref="ue"></UE>
-
-      　　</section>
-
-
+        <el-button class="editor-btn" type="primary" @click="getUEContent">保存</el-button>
+        <el-button plain size="mini" @click="indexCaseShow">取消</el-button>
+        <!--<button @click="getUEContent()">获取内容</button>-->
+      </div>
     </div>
   </div>
 </template>
 <script>
-  import UE from './ue.vue';
+  // import UE from './ue.vue';
   import {mapActions} from 'vuex'
 
   export default {
@@ -33,12 +33,13 @@
       UE
       // quillEditor
     },
-    mounted(){
-      this.title = this.navIndex.title;
-
-    },
+    // mounted(){
+    //   this.title = this.navIndex.title;
+    //
+    // },
     data() {
       return {
+        editor: null,
         title:"",
         // content: '',
         // editorOption: {
@@ -52,7 +53,21 @@
         }
       }
     },
+    mounted() {
+      this.title = this.navIndex.title;
+
+      const _this = this;
+      this.editor = UE.getEditor("ueid", this.config); // 初始化UE
+      this.editor.addListener("ready", function () {
+        _this.editor.setContent(_this.defaultMsg); // 确保UE加载完成后，放入内容。
+      });
+    },
     methods: {
+      getUEContent() {    // 获取内容方法
+        console.log(this.editor.getContent())
+        return this.editor.getContent()
+      },
+
       // onEditorChange({editor, html, text}) {
       //   this.content = html;
       // },
@@ -71,6 +86,9 @@
         ["indexCaseShow"]
       ),
 
+    },
+    destroyed() {
+      this.editor.destroy();
     }
   }
 </script>
@@ -80,6 +98,10 @@
     margin: 10px 0;
     font-size: 18px;
     color: #555;
+  }
+
+  .editor-btn {
+    margin-top: 20px;
   }
 </style>
 <style>
