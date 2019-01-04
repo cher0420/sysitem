28695d5f4225c5cabc22b8c29c74efc66ae7d38e
Eric <17621869522@163.com>
2019-01-04 16:40:40 +0800

    页脚备案 位置调整

---
+++ src/page/webtalk/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <iframe :src="url" frameborder="0" style="width: 100%;height: 100%;"></iframe>
+</template>
+<style scoped>
+</style>
+<script>
+  import URL from '../../host/baseUrl'
+
+  export default{
+    data () {
+      return {
+        url: URL.baseUrl + '/WebTalk/Index.html'
+      }
+    },
+    methods: {
+    }
+  }
+</script>
