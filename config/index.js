483020372ca20a7db2f844a7a10374d68fbc42de
xuxue <xuxue@17shihui.com>
2019-03-06 17:32:37 +0800

    增加列表接口

--- config/index.js
+++ config/index.js
@@ -32,7 +32,7 @@ module.exports = {
         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
       },
       '/api/admin/keyword':{
-        target: 'https://192.168.1.103:10037',// 接口的域名
+        target: 'https://192.168.1.103:10036',// 接口的域名
         secure: false,// 如果是https接口，需要配置这个参数
         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
       },
--- src/constants/api.js
+++ src/constants/api.js
@@ -40,7 +40,7 @@ export const QUERYQADATA = "/api/admin/portal/qqa/querybykeyword"; //  快速问
 export const STOREQADATA = "/api/admin/portal/qqa/save"; // 快速问答存储答案
 
 // 关键词回复
-export const KEYWORDLIST = '/api/admin/channel/v2/webchat/getskillintent';//关键词列表
+export const KEYWORDLIST = '/api/admin/keyword/KQA/BotSearch';//关键词列表
 export const KEYWORDLEADEXCEL = '/api/admin/keyword/KQA/upload';//导入关键词
 export const KEYWORDDOWNLOAD = '';//下载关键词模版
 export const DELETEKEYWORD = '';//删除关键词
