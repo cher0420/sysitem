a0e1a427ff247eb55c2bcafe7b7ed7647a45218d
xuxue <xuxue@17shihui.com>
2019-04-28 18:09:18 +0800

    dashboard完成

--- src/page/dashboard/index.vue
+++ src/page/dashboard/index.vue
@@ -17,7 +17,7 @@
             <i class="user icon"></i>
             <span class="title">昨日用户访问量</span>
           </div>
-          <div v-if='Data.SaveTime' class="content">
+          <div v-if='Data.hasNum' class="content">
             <div class="count">
               <span class="user-primary-color">{{Data.VisitCount}}</span>
               <span>人</span>
@@ -45,7 +45,7 @@
             <i class="interaction icon"></i>
             <span class="title">昨日人机交互量</span>
           </div>
-          <div v-if='Data.SaveTime' class="content">
+          <div v-if='Data.hasNum' class="content">
             <div class="count">
               <span class="interaction-primary-color">{{Data.InteractCount}}</span>
               <span>次</span>
@@ -74,7 +74,7 @@
             <i class="time icon"></i>
             <span class="title">累计昨日节约时间</span>
           </div>
-          <div class="content" v-if="Data.SaveTime" >
+          <div class="content" v-if="Data.hasNum" >
             <div class="count">
               <span class="save-primary-color">{{Data.SaveTimeHours <= 99999? Data.SaveTime <= 99999?Data.SaveTime:Data.SaveTimeHours:(Data.SaveTimeHours/10000).toFixed()}}</span>
               <span>{{Data.SaveTimeHours<=99999?Data.SaveTime <= 99999?'分钟':'小时':'万小时'}}</span>
@@ -98,9 +98,9 @@
             <i class="question icon"></i>
             <span class="title">昨日提问分布</span>
           </div>
-          <div id="myChart" v-show='Data.SaveTime'>
+          <div id="myChart" v-show="Data.hasNum">
           </div>
-          <div v-show='Data.SaveTime <= 0' class="no-question-data">
+          <div v-show='!Data.hasNum' class="no-question-data">
             <ul>
               <li><span></span>专业知识/业务咨询</li>
               <li><span></span>闲聊</li>
@@ -120,11 +120,16 @@
             <i class="hot icon"></i>
             <span class="title">昨日热点问题</span>
           </div>
-          <div v-if='Data.SaveTime' class="hot-list">
-              <div v-for="(item, index) in hotQuestionList" :key="index" >
-              <span>
-                {{item.FriendlyName}}
-              </span>
+          <div v-if='Data.hasNum' class="hot-list" id="hotList">
+              <div v-for="(item, index) in hotQuestionList" :key="index">
+                <el-tooltip v-if='item.showTooltip' :content="item.FriendlyName" placement="top" effect="dark">
+                  <span style="width: 85%;overflow: hidden;text-overflow:ellipsis;white-space: nowrap;">
+                    {{item.FriendlyName}}
+                  </span>
+                </el-tooltip>
+                <span v-else style="width: 85%;overflow: hidden;text-overflow:ellipsis;white-space: nowrap;">
+                    {{item.FriendlyName}}
+                  </span>
                 <span>
                 <span :class="isRise(item.Rise, 'hot-icon')"></span>
               </span>
@@ -147,15 +152,15 @@
             <div slot="header">
               <i class="unknown icon"></i>
               <span class="title">昨日未知问题数量</span>
-              <el-button :disabled="Data.UnknowQANum<=0|| !Data.SaveTime" type="text" class="downLoad">
-                <a :href="Data.UnknowQANum<=0|| !Data.SaveTime?'javascript:;':Data.UnknownQFileUrl"
-                   :style="{opacity:Data.UnknowQANum<=0|| !Data.SaveTime?'.5':'1',
-                            cursor:Data.UnknowQANum<=0|| !Data.SaveTime?'default':'pointer'
+              <el-button :disabled="Data.UnknowQANum<=0|| !Data.hasNum" type="text" class="downLoad">
+                <a :href="Data.UnknowQANum<=0|| !Data.hasNum?'javascript:;':Data.UnknownQFileUrl"
+                   :style="{opacity:Data.UnknowQANum<=0|| !Data.hasNum?'.5':'1',
+                            cursor:Data.UnknowQANum<=0|| !Data.hasNum?'default':'pointer'
                    }"
                 >问题导出</a>
               </el-button>
             </div>
-            <div v-if='Data.SaveTime' class="total">
+            <div v-if='Data.hasNum' class="total">
               <span>{{Data.UnknowQANum}}</span>
               <span>个</span>
             </div>
@@ -166,34 +171,34 @@
           </el-card>
         </el-row>
         <el-row class="margin-top-20">
-          <el-card class="box-card port-visitor" body-style="height:170px;box-sizing:border-box;position:relative;" v-loading="loading">
+          <el-card class="box-card port-visitor" body-style="height:168px;box-sizing:border-box;position:relative;" v-loading="loading">
             <div slot="header">
               <i class="port icon"></i>
               <span class="title">昨日端口访问量</span>
             </div>
-            <div v-if="Data.SaveTime">
+            <div v-if="Data.hasNum">
               <div class="progress">
-              <span :style="{width:Data.WechatChannelNumPrecent}">
-                <span>{{Data.WechatChannelNumPrecent}}</span>
-                <span :style="{height:'15px',width:'100%',background:'#288288'}"></span>
-              </span>
+                <span :style="{width:Data.WechatChannelNumPrecent}">
+                  <span v-show="Data.WechatChannelNum" >{{Data.WechatChannelNumPrecent}}</span>
+                  <span :style="{height:'15px',width:'100%',background:'#0d575c'}"></span>
+                </span>
                 <span :style="{width:Data.WebChannelNumPrecent}">
-                <span>{{Data.WebChannelNumPrecent}}</span>
-                <span :style="{height:'15px',width:'100%',background:'#24a5ae'}"></span>
-              </span>
-                <span :style="{width:Data.RobotChannelNumPrecent}">
-                <span>{{Data.RobotChannelNumPrecent}}</span>
-                <span :style="{height:'15px',width:'100%',background:'#2ec6d1'}"></span>
-              </span>
+                  <span v-show="Data.WebChannelNum" style="transform:translateX(5px)">{{Data.WebChannelNumPrecent}}</span>
+                  <span :style="{height:'15px',width:'100%',background:'#288288'}"></span>
+                </span>
                 <span :style="{width:Data.MiniProgramChannelNumPrecent}">
-                <span>{{Data.MiniProgramChannelNumPrecent}}</span>
-                <span :style="{height:'15px',width:'100%',background:'#8fe0e6'}"></span>
-              </span>
+                  <span v-show="Data.MiniProgramChannelNum" style="transform:translateX(5px)">{{Data.MiniProgramChannelNumPrecent}}</span>
+                  <span :style="{height:'15px',width:'100%',background:'#8fe0e6'}"></span>
+                </span>
+                <span :style="{width:Data.RobotChannelNumPrecent}">
+                  <span v-show="Data.RobotChannelNum" style="transform:translateX(5px)">{{Data.RobotChannelNumPrecent}}</span>
+                  <span :style="{height:'15px',width:'100%',background:'#2ec6d1'}"></span>
+                </span>
               </div>
               <div class="tooltip">
-                <span><i style="background: #288288"></i>微信端</span>
-                <span><i style="background: #24a5ae"></i>网页端</span>
-                <span><i style="background: #2ec6d1"></i>桌面</span>
+                <span><i style="background: #0d575c"></i>微信端</span>
+                <span><i style="background: #288288"></i>网页端</span>
+                <span><i style="background: #2ec6d1"></i>小程序</span>
                 <span><i style="background: #8fe0e6"></i>实体机器人</span>
               </div>
             </div>
@@ -228,9 +233,11 @@
       return {
         BotConfigId: null,
         Data: {
+          hasNum: true,
           VisitCount: 0,
           InteractCount: 0,
           SaveTime: 0,
+          SaveTimeHours: 0,
           ProfessionQANum: '',
           ChatQANum: '',
           UnknowQANum: 0,
@@ -312,7 +319,6 @@
       this.getHotQA()
     },
     mounted() {
-
     },
     components:{
       NavTitle,
@@ -332,7 +338,12 @@
           headers:{
             'Access-Token': getCookies(TOKEN)
           },
-          body:JSON.stringify({TenantId:TenantId,BotConfigId:this.BotConfigId,"PreStartDate":"2019-4-1 00:00:00",
+          body:JSON.stringify({
+            // TenantId:TenantId,
+            // BotConfigId:this.BotConfigId,
+            "TenantId":"60124b8f-4355-4c4e-a9bf-6286b434d8c4",
+            "BotConfigId":"huajing0-181a-46bd-94b2-8a5b5cestage",
+            "PreStartDate":"2019-4-1 00:00:00",
             "PreEndDate":"2019-4-10 23:59:59",
             "CurrStartDate":"2019-4-11 00:00:00",
             "CurrEndDate":"2019-4-21 23:59:59"})
@@ -340,11 +351,14 @@
         request(HOTQA, params).then(
           (res) =>{
             this.hotQuestionList = []
+            res.Data[0].FriendlyName = '生育保险待遇_领取_查询办理生育保险待遇_领取_查询办理生育保险待遇_领取_查询办理生育保险待遇_领取_查询办理生育保险待遇_领取_查询办理'
+
             res.Data.forEach(
               (item, index, arr) => {
                 if(item.Ranking === index+1){
                   this.hotQuestionList.push(item)
                 }
+                item.showTooltip = item.FriendlyName.length > that.computedFontLength()
               }
             )
             setTimeout(
@@ -368,16 +382,22 @@
           headers:{
             'Access-Token': getCookies(TOKEN)
           },
-          body:JSON.stringify({TenantId:TenantId,BotConfigId:this.BotConfigId,"PreStartDate":"2019-4-1 00:00:00",
+          body:JSON.stringify({
+            // TenantId:TenantId,
+            // BotConfigId:this.BotConfigId,
+            "TenantId":"60124b8f-4355-4c4e-a9bf-6286b434d8c4",
+            "BotConfigId":"huajing0-181a-46bd-94b2-8a5b5cestage",
+            "PreStartDate":"2019-4-1 00:00:00",
             "PreEndDate":"2019-4-10 23:59:59",
             "CurrStartDate":"2019-4-11 00:00:00",
-            "CurrEndDate":"2019-4-21 23:59:59"})
+            "CurrEndDate":"2019-4-21 23:59:59"
+          })
         }
         request(STATUSMETRICS, params).then(
           (res) =>{
             // res.Data = null
             const data = res.Data?res.Data:this.Data
-
+            data.hasNum = data.SaveTime > 0
             data.WechatChannelNumPrecent = (data.WechatChannelNum/( data.WechatChannelNum+ data.WebChannelNum+data.RobotChannelNum+data.MiniProgramChannelNum)*100).toFixed()+'%'
             data.WebChannelNumPrecent = (data.WebChannelNum/( data.WechatChannelNum+ data.WebChannelNum+data.RobotChannelNum+data.MiniProgramChannelNum)*100).toFixed()+'%'
             data.RobotChannelNumPrecent = (data.RobotChannelNum/( data.WechatChannelNum+ data.WebChannelNum+data.RobotChannelNum+data.MiniProgramChannelNum)*100).toFixed()+'%'
@@ -393,8 +413,6 @@
               },500
             )
             this.drawLine(this.Data);
-
-
           }
         ).catch(
           () => {
@@ -437,14 +455,14 @@
               position : function (point, params, dom, rect, size) {
                 // 固定在顶部
                 const domWidth = dom.offsetWidth
-                return [size.viewSize[0]/2-domWidth/15,size.viewSize[1]/2-10];
+                return [size.viewSize[0]/2-18,size.viewSize[1]/2-15];
               },
               textStyle:{
                 textAlign: 'center',
                 width: 200,
                 height: 50
               },
-              formatter: '<div><span style="color: #2a8be7; font-size: 30px">'+'{c}'+'</span><span style="color: #555; font-size: 16px">条</span></div>',
+              formatter: '<div style="position: absolute;left: 50%;top: 50%;"><span style="color: #2a8be7; font-size: 30px">'+'{c}'+'</span><span style="color: #555; font-size: 16px">条</span></div>',
             },
             legend: {
               orient: 'vertical',
@@ -462,28 +480,28 @@
             },
             series: [
               {
-                center:['62%', '51%'],
+                center:['55%', '50%'],
                 name:'sss',
                 type:'pie',
-                radius: ['40%', '60%'],
+                radius: ['35%', '50%'],
                 avoidLabelOverlap: false,
                 label: {
                   normal: {
-                    show: true,
+                    show: false,
                     position:'left',
                     textStyle: {
-                      fontSize: 14,
+                      fontSize: 12,
                       color: '#555'
                     },
                     formatter(params){
-                      const value = (params.value/(data.ProfessionQANum + data.ChatQANum + data.UnknowQANum)*100).toFixed()+'%'
+                      const value = params.percent.toFixed()+'%'
                       return [value, params.name].join('\n')
                     }
                   },
                   emphasis: {
                     show: true,
                     formatter(params){
-                      const value = (params.value/(data.ProfessionQANum + data.ChatQANum + data.UnknowQANum)*100).toFixed()+'%'
+                      const value = params.percent.toFixed()+'%'
                       return [value, params.name].join('\n')
                     }
                   }
@@ -500,6 +518,9 @@
                   {value:data.UnknowQANum, name:'未知问题', itemStyle:{normal:{color:'#999999'},emphasis:{color:'#999999'}}},
                   {value:data.ChatQANum, name:'闲聊', itemStyle:{normal:{color:'#f39504'},emphasis:{color:'#f39504'}}},
                   {value:data.ProfessionQANum, name:'专业知识/业务咨询', itemStyle:{normal:{color:'#2a8be7'},emphasis:{color:'#2a8be7'}}},
+                  // {value:110, name:'未知问题', itemStyle:{normal:{color:'#999999'},emphasis:{color:'#999999'}}},
+                  // {value:5, name:'闲聊', itemStyle:{normal:{color:'#f39504'},emphasis:{color:'#f39504'}}},
+                  // {value:420, name:'专业知识/业务咨询', itemStyle:{normal:{color:'#2a8be7'},emphasis:{color:'#2a8be7'}}},
                 ]
               }
             ]
@@ -516,6 +537,11 @@
         }else{
           return ['keep', className]
         }
+      },
+      computedFontLength(){
+        const ele = document.getElementById('hotList');
+        const length = ele.clientWidth*0.85/16
+        return Math.floor(length)
       }
     }
   }
@@ -821,7 +847,15 @@
     }
   }
   #myChart{
-    height: 100%;width:100%;position: absolute;right: 0;top: 40px;
+    height: 100%;
+    width:100%;
+    position: absolute;
+    left: 0;
+    top: 40px;
+    div{
+      width: 100% !important;
+      height: 100% !important;
+    }
   }
   @media screen and (max-width: 1440px) {
     .save-box,.unknown-box,.port-box{
